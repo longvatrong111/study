9310aba66641716d9a1f5503409f3e99cf2b1c1e
TRONG BINH HOANG/LGEDV webOS TEAM(trong.hoang@lge.com) <trong.hoang@lge.com>
2023-08-22 23:22:13 +0700

    README

--- Competition/competition.cpp
+++ Competition/competition.cpp
@@ -8,70 +8,18 @@ using namespace std;
 /* global variables */
 int T;
 
-long long matrixSumQueries(int n, vector<vector<int>> &quer)
-{
-    long long ans = 0;
-    set<int> row;
-    set<int> col;
-    int nRow = n, nCol = n;
-
-    int q = quer.size();
-    int type, idx;
-    long long val;
-    for (int i = q - 1; i >= 0; --i)
-    {
-        type = quer[i][0];
-        idx = quer[i][1];
-        val = quer[i][2];
-
-        if (type == 0)
-        {
-            if (row.count(idx))
-                continue;
-            row.insert(idx);
-
-            ans += val * nCol;
-
-            nRow--;
-        }
-        else
-        {
-            if (col.count(idx))
-                continue;
-            col.insert(idx);
-
-            ans += val * nRow;
-
-            nCol--;
-        }
-    }
-
-    return ans;
-}
-
 /* main function */
-int32_t main()
-{
+int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    // cin >> T;
-    T = 1;
+    cin >> T;
 
-    while (T--)
-    {
-        int n, q;
-        cin >> n >> q;
-        vector<vector<int>> query;
-        for (int i = 0; i < q; ++i)
-        {
-            int x, y, z;
-            cin >> x >> y >> z;
-            query.push_back({x, y, z});
-        }
-
-        cout << matrixSumQueries(n, query) << endl;
+    while (T--) {
+        int n; cin >> n;
+        for (int i = 0; i < n; ++i) cout << i << " ";
+        cout << endl;
     }
 
     return 0;
